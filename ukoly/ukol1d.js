250a1e88ecbf922ad96875978675229f71d185d5
Petr Vecera <petr.vecera@profiq.com>
2021-05-01 08:58:11 +0200

    ukol1d

---
+++ ukoly/ukol1d.js
@@ -0,0 +1,49 @@
+/**
+ * 1d) Vypis na obrazovku prvni prvek pole1 a prvni prvek pole2
+ * 
+ * Porovnej prvni prvky pole a vypis na obrazovku bud:
+ * "Prvni prvky jsou stejne!" a nebo "Prvni prvky nejsou stejne!"
+ * 
+ * Napoveda: podivej se do examples/pole.js
+ * 
+ */
+
+
+let pole1 = [7,9,5,7,6]
+let pole2 = [7,9,5,4,6]
+
+
+/**
+ * 1dd)
+ * Napis kod ktery zkontroluje ze jsou pole stejna za pouziti for cyklu.
+ * Tedy: Napiseme si for cyklus ktery nam bude prochazet obe pole zaroven a budeme postupne
+ * porovnavat kazdy prvek pole. Tedy prvek z pole1 a pole2 na stejnem indexu. 
+ * 
+ * Pokud potrebujes napovedu, podivej se do: examples/iterace-polem.js
+ * 
+ * Vysledek cheme vypsat az po skonceni for cyklu. Tedy az vsechno skonci vypis:
+ * "Pole jsou stejne" nebo "pole nejsou stejne"
+ * 
+ */
+
+
+
+
+/**
+ * 1ddd)
+ * Vylepsi predchozi kod, tak ze budeme ve vysledku znat i index, kde prvky nejsou stejne.
+ * (BONUS: Vypis i prvky ktere nejsou stejne).
+ * 
+ * Vysledek:
+ * "Pole jsou stejne"
+ *  nebo
+ * "Pole nejsou stejne na indexu 4"
+ */
+
+
+
+
+/**
+ * 1dddd)
+ * Presun svuj kod do funkce, ktera bude prijimat dve pole a bude cravet true nebo false, podle toho jestli jsou pole stejne nebo ne.
+ */
